870aa240b12dba0f014c2cccdec683daffc5dac3
Iliya Shafirov <kamy.huo@kippbastudent.org>
2015-07-28 15:08:36 -0700

    Adding all files

---
+++ game.js
@@ -0,0 +1,168 @@
+enchant();
+var maxWaterLevel = 20;
+var time = 120;
+var timeSinceLastAlert = 0;
+var timeToNewAlert = 20;
+var alertArray = ["Africa's economy has fallen!", "1 million people have died!", "Tornadoes destroy water lines"];
+var i = 0;
+updateTime = function(){
+	time = time - 1;
+	timeSinceLastAlert = timeSinceLastAlert + 1;
+};
+window.setInterval(updateTime, 1000);
+window.onload = function(){
+  var game = new Game(300, 300);
+  game.spriteSheetWidth = 672;
+  game.spriteSheetHeight = 24;
+  game.fps = 15;
+  game.spriteWidth = 24;
+  game.spriteHeight = 24;
+  game.preload('sprites.png');
+  game.preload('water.png');
+  var map = new Map(game.spriteWidth, game.spriteHeight);
+  var foregroundMap = new Map(game.spriteWidth, game.spriteHeight);
+  var puddleMap = new Map(game.spriteWidth, game.spriteHeight);
+  var setMaps = function(){
+    map.image = game.assets['sprites.png'];
+    map.loadData(mapData);
+    foregroundMap.image = game.assets['sprites.png'];
+    foregroundMap.loadData(foregroundData);
+	puddleMap.image = game.assets['sprites.png'];
+	puddleMap.loadData(puddle);
+    var collisionData = [];
+    for(var i = 0; i< foregroundData.length; i++){
+      collisionData.push([]);
+      for(var j = 0; j< foregroundData[0].length; j++){
+        var collision = foregroundData[i][j] === 1 ? 1 : 0;
+        collisionData[i][j] = collision;
+      }
+    }
+    map.collisionData = collisionData;
+  };
+  var alert = function(){
+	  if(timeSinceLastAlert >= timeToNewAlert){
+		  confirm(alertArray[i]);
+		  i += 1;
+		  timeSinceLastAlert = 0;
+	  }
+  };
+  var waterLoss = function(){
+	  if(player.waterSupply < 20){
+		  player.waterSupply = player.waterSupply + 1;
+	  }
+	  else if (player.waterSupply >= 20){
+		  confirm("You died!");
+		  clearInterval(waterLevelLoss);
+		  game.stop();
+	  }
+  };
+  var waterLevelLoss = window.setInterval(waterLoss,2000);
+  var setStage = function(){
+    var stage = new Group();
+    stage.addChild(map);
+    stage.addChild(player);
+	stage.addChild(waterLevel);
+    stage.addChild(foregroundMap);
+	stage.addChild(puddleMap);
+    game.rootScene.addChild(stage);
+  };
+  
+  var name = prompt("What is your name?");
+  
+  var waterLevel = new Sprite(game.spriteWidth, game.spriteHeight);
+  var setWaterLevel = function(){
+	  waterLevel.spriteOffset = 7;
+	  waterLevel.x = player.x + 1;
+	  waterLevel.y = player.y - 20;
+	  waterLevel.frame = waterLevel.spriteOffset + player.waterSupply;
+	  waterLevel.scale(1.5,1.5);
+	  waterLevel.image = new Surface(game.spriteSheetWidth, game.spriteSheetHeight);
+	  waterLevel.image.draw(game.assets['sprites.png']);
+  };
+  waterLevel.move = function(){
+	  waterLevel.x = player.x + 1;
+	  waterLevel.y = player.y - 20;
+	  waterLevel.frame = waterLevel.spriteOffset + player.waterSupply;
+  };
+  var player = new Sprite(game.spriteWidth, game.spriteHeight);
+  var setPlayer = function(){
+    player.spriteOffset = 2;
+    player.startingX = 0;
+    player.startingY = 20;
+    player.x = player.startingX * game.spriteWidth;
+    player.y = player.startingY * game.spriteHeight;
+    player.direction = 0;
+    player.walk = 0;
+    player.frame = player.spriteOffset + player.direction; 
+    player.image = new Surface(game.spriteSheetWidth, game.spriteSheetHeight);
+    player.image.draw(game.assets['sprites.png']);
+    player.name = name;
+	player.waterSupply = maxWaterLevel/2;
+  };
+  player.move = function(){
+    this.frame = this.spriteOffset + this.direction * 2 + this.walk;
+    if (this.isMoving) {
+      this.moveBy(this.xMovement, this.yMovement);
+      if ((this.xMovement && this.x % 24 === 0) || (this.yMovement && this.y % 24 === 0)) {
+        this.isMoving = false;
+      } 
+    } else {
+      this.xMovement = 0;
+      this.yMovement = 0;
+      if (game.input.up) {
+        this.direction = 0;
+        this.yMovement = -4;
+		this.walk = 1;
+      } else if (game.input.right) {
+        this.direction = 1;
+        this.xMovement = 4;
+		this.walk = 0;
+      } else if (game.input.left) {
+        this.direction = 1;
+        this.xMovement = -4;
+		this.walk = 1;
+      } else if (game.input.down) {
+        this.direction = 0;
+        this.yMovement = 4;
+		this.walk = 0;
+      }
+      if (this.xMovement || this.yMovement) {
+        var x = this.x + (this.xMovement ? this.xMovement / Math.abs(this.xMovement) * 24 : 0);
+        var y = this.y + (this.yMovement ? this.yMovement / Math.abs(this.yMovement) * 24 : 0);
+      if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
+          this.isMoving = true;
+          this.move();
+        }
+      }
+    }
+  };
+  game.focusViewport = function(){
+    var x = Math.min((game.width  - 24) / 2 - player.x, 0);
+    var y = Math.min((game.height - 24) / 2 - player.y, 0);
+    x = Math.max(game.width,  x + map.width)  - map.width;
+    y = Math.max(game.height, y + map.height) - map.height;
+    game.rootScene.firstChild.x = x;
+    game.rootScene.firstChild.y = y;
+  };
+  game.timer = function(){
+	var timer = 3;
+	timer.setInterval(game.timer, 1000);
+
+	  
+  }
+  game.onload = function(){
+    setMaps();
+    setPlayer();
+    setStage();
+	setWaterLevel();
+    player.on('enterframe', function() {
+      player.move();
+	  waterLevel.move();
+	  alert();
+    });
+    game.rootScene.on('enterframe', function(e) {
+      game.focusViewport();
+    });
+  };
+  game.start();
+};
