ef88d7fbb5debf6236b626c62b20a6ae5097dbde
17yuhansamantha <17150@mychs.ca>
2015-07-29 13:16:12 -0700

    water works

--- game.js
+++ game.js
@@ -172,13 +172,11 @@ window.onload = function(){
   var checkPuddle = function(){
 	 
 	 if (puddleMap.checkTile(player.x, player.y) === 6){
-		 player.waterSupply = player.waterSupply - 1;
-		 console.log("IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		for(var k = 0; k< puddle.length; k++){
-			for(var l = 0; l< puddle[k].length; l++){
-				puddle[k][l] = -1;
-			}
-		}
+		player.waterSupply = player.waterSupply - 1;
+		console.log("IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		var k = player.x / 24 | 0;
+		var l = player.y / 24 | 0;
+		puddle[k][l] = -1;
 	}
 	 
   };
